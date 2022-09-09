72982f8b053d7fcd10f16885752023d0063cf77e
arudrra <45153523+arudrra@users.noreply.github.com>
2022-09-08 19:34:17 -0700

    toggle tags working

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
+const FIELDLENGTH = 15
 // const generateButton = document.getElementById("drag-page")
-const input = document.getElementById("main-input")
+const input = document.getElementById("main-input");
 // const conditional = document.getElementById("conditional")
 // generateButton.addEventListener("click", generate);
+const toggleButton = document.getElementById("toggle-control-button");
+toggleButton.addEventListener("click", createToggleSyntax);
+const customButton = document.getElementById("custom-control-button");
+customButton.addEventListener("click", createCustomSyntax());
 
 function generate() {
-    console.log(input.selectionStart +" " + input.selectionEnd)
-    console.log(input.value.substring())
-    input.value = "YEET"
+    console.log(input.selectionStart +" " + input.selectionEnd);
+    console.log(input.value.substring());
+    input.value = "YEET";
 }
 
 var heightLimit = 200; /* Maximum height: 200px */
@@ -16,3 +21,35 @@ input.oninput = function() {
 //   input.style.height = Math.min(input.scrollHeight, heightLimit) + "px";
   input.style.height = input.scrollHeight + "px";
 };
+
+//Splits 
+function splitOnSelection(text, start, end){
+    var prefix = text.slice(0,start);
+    var middle = text.slice(start, end);
+    var suffix = text.slice(end);
+    return [prefix, middle, suffix];
+}
+
+function createToggleSyntax(){
+    if (input.selectionStart != input.selectionEnd) {
+        const sections = splitOnSelection(input.value.substring(), input.selectionStart, input.selectionEnd);
+        input.value = sections[0] + "<t>" + sections[1] + "</t>" + sections[2];
+    } else {
+        console.log("invalid toggle creation");
+    }
+}
+
+function createCustomSyntax(){
+    const start = input.selectionStart;
+    const end = input.selectionEnd;
+    if (start != end) {
+        if (end - start <= FIELDLENGTH) {
+            const sections = splitOnSelection(input.value.substring(), start, end);
+            input.value = sections[0] + "<c id=" +sections[1] + ">" + sections[1] + "</c>" + sections[2];
+        } else {
+            console.log("invalid custom creation");
+        }
+    } else {
+
+    }
+}
