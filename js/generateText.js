975cf67675fef5de99705eefcf1f01df9c52c6fe
arudrra <45153523+arudrra@users.noreply.github.com>
2022-09-16 16:20:26 -0700

    added some second page functionality such as start up and progression to next decision

--- js/generateText.js
+++ js/generateText.js
@@ -1,11 +1,39 @@
-document.addEventListener('DOMContentLoaded', generate);
+//Globals
+let currentText;
+let previousActions;
+let remainingSegments;
 
+//Useful Elements
+const generatedTextArea = document.getElementById("generated-text");
+
+//Return to Template page (go back button)
 const returnToTemplateButton = document.getElementById("return-to-template");
 returnToTemplateButton.addEventListener("click", returnToTemplate);
 function returnToTemplate() {
     window.open("template.html", "_self");
 }
 
-function generate() {
-    alert("Ready!");
+//Loads template from storage
+function load() {
+    currentText = [];
+    previousActions = [];
+    remainingSegments = JSON.parse(localStorage.parsedTemplate);
+}
+
+//Automatically adds text that does not require user decisions until it reaches a user decision
+function advanceToNextDecision() {
+    index = 0;
+    while (index < remainingSegments.length && remainingSegments[index].type == 0) {
+        //sometimes, blank characters get appended
+        generatedTextArea.innerHTML = remainingSegments[index].text;
+        index += 1;
+    }
+}
+
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", startGenerator);
+
+function startGenerator() {
+    load();
+    advanceToNextDecision();
 }
