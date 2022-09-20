92c9913c353d74282f9b7f8986505edb40afd406
arudrra <45153523+arudrra@users.noreply.github.com>
2022-09-20 10:58:25 -0700

    working toggles on second page

--- js/generateText.js
+++ js/generateText.js
@@ -13,20 +13,78 @@ function load() {
     ParsedText = new Object();
     //String.raw necessary for firefox since \n is evaluated otherwise
     ParsedText.segments = JSON.parse(String.raw`${sessionStorage.parsedTemplate}`);
-    console.log(ParsedText);
     ParsedText.index = 0;
 }
 
+//Creates the the toggle interface for the user to decide on a toggle
+function createToggleInterface(parent, child) {
+    const generatorControlPanel = document.getElementById("generator-control-panel");
+    const toggleInterfacePanel = document.createElement("div");
+    toggleInterfacePanel.setAttribute("id", "toggle-interface-panel");
 
+    const backButton = document.createElement("button");
+    backButton.className = "bar-button left-control-button back-button";
+    backButton.innerText = "< back";
+
+    const toggleExclude = document.createElement("button")
+    toggleExclude.className = "bar-button";
+    toggleExclude.innerText = "exclude";
+    toggleExclude.addEventListener("click", function() {
+        if (parent.contains(child)) {
+            parent.removeChild(child);
+        }
+    });
+    //back-button has the red hover color
+    // toggleExclude.setAttribute("back-button");
+
+    const toggleInclude = document.createElement("button")
+    toggleInclude.className = "bar-button";
+    toggleInclude.innerText = "include";
+    toggleInclude.addEventListener("click", function() {
+        if (!parent.contains(child)) {
+            parent.appendChild(child);
+        }
+    });
+
+    const nextButton = document.createElement('button');
+    nextButton.className = "bar-button right-control-button next-button";
+    nextButton.innerText = "next >";
+    nextButton.addEventListener("click", function() {
+        //Remove the interface for toggling
+        generatorControlPanel.removeChild(toggleInterfacePanel);
+        toggleInterfacePanel.removeChild(backButton);
+        toggleInterfacePanel.removeChild(toggleExclude);
+        toggleInterfacePanel.removeChild(toggleInclude);
+        toggleInterfacePanel.removeChild(nextButton);
+        toggleInterfacePanel.remove();
+        backButton.remove();
+        toggleExclude.remove();
+        toggleInclude.remove();
+        nextButton.remove();
+        //Advance to next decision
+        advanceToNextDecision();
+    })
+
+
+    toggleInterfacePanel.appendChild(backButton);
+    toggleInterfacePanel.appendChild(toggleExclude);
+    toggleInterfacePanel.appendChild(toggleInclude);
+    toggleInterfacePanel.appendChild(nextButton);
+    generatorControlPanel.appendChild(toggleInterfacePanel);
+    
+
+}
+
+//If the toggle is chosen
 function toggleDecision() {
-    //If the toggle is chosen
     const generatedTextArea = document.getElementById("generated-text");
     span = document.createElement("span");
     span.className = "toggle-text";
     span.textContent = ParsedText.segments[ParsedText.index].text;
     generatedTextArea.appendChild(span);
     ParsedText.index += 1;
-    advanceToNextDecision();
+    createToggleInterface(generatedTextArea, span);
+    // advanceToNextDecision();
 }
 
 function customDecision() {
